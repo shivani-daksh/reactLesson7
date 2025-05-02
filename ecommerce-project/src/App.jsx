45cea0b03068807591d7715f3e61f5268716a04b
shivani daksh <shivanidaksh2311@gmail.com>
2025-05-02 15:33:48 +0530

    starting code for Lecture7

---
+++ ecommerce-project/src/App.jsx
@@ -0,0 +1,24 @@
+import {Routes, Route} from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
+import { NotFoundPage } from './pages/NotFoundPage'
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<NotFoundPage />} />  
+    </Routes>
+    
+  )
+}
+
+export default App
