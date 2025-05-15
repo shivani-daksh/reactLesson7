39f32bc6d72c0fe4805adbcbb10b5b1aaaa2f909
shivani daksh <shivanidaksh2311@gmail.com>
2025-05-15 21:00:17 +0530

    Pass the cart to display total Quantity(7g)

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router'
 import "./CheckoutHeader.css"
 
-export function CheckoutHeader() {
+export function CheckoutHeader({cart}) {
+  let totalQuantity = 0;
+
+cart.forEach((cartItem) => {
+  totalQuantity += cartItem.quantity;
+});
+
   return (
     <div className="checkout-header">
         <div className="header-content">
@@ -15,7 +21,7 @@ export function CheckoutHeader() {
           <div className="checkout-header-middle-section">
             Checkout (
             <Link className="return-to-home-link" to="/">
-              3 items
+              {totalQuantity} items
             </Link>
             )
           </div>
--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -31,7 +31,7 @@ export function CheckoutPage({ cart }) {
       <title>Checkout</title>
       <link rel="icon" type="image/png" href="cart-favicon.png" />
 
-      <CheckoutHeader />
+      <CheckoutHeader cart={cart} />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
